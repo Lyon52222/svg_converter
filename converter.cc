ddf590caded3d5c1037d0f9b8bce69a88acc789a
lyon <1765813715@qq.com>
2022-09-09 17:21:38 +0800

    add line rect poly

--- converter.cc
+++ converter.cc
@@ -3,7 +3,7 @@
 
 void Circle2Path(int cx, int cy, int r, char *path) { path[0] = 'M'; }
 
-void Line2Path(int x1, int y1, int x2, int y2, char *path) {
+int Line2Path(int x1, int y1, int x2, int y2, char *path) {
     path[0] = 'M';
     path[1] = x1;
     path[2] = ',';
@@ -13,9 +13,11 @@ void Line2Path(int x1, int y1, int x2, int y2, char *path) {
     path[6] = ',';
     path[7] = y2;
     path[8] = 'z';
+
+    return 8;
 }
 
-void Rect2Path(int x, int y, int width, int heigth, char *path) {
+int Rect2Path(int x, int y, int width, int heigth, char *path) {
     path[0] = 'M';
     path[1] = x;
     path[2] = ',';
@@ -25,6 +27,27 @@ void Rect2Path(int x, int y, int width, int heigth, char *path) {
     path[6] = ',';
     path[7] = y + width;
     path[8] = 'z';
+
+    return 8;
+}
+
+int Poly2Path(int **points, int size_points, char *path) {
+    int t = 4;
+    path[0] = 'M';
+    path[1] = points[0][0];
+    path[2] = ',';
+    path[3] = points[0][1];
+
+    for (int i = 1; i < size_points; i++) {
+        path[t++] = 'L';
+        path[t++] = points[i][0];
+        path[t++] = ',';
+        path[t++] = points[i][1];
+    }
+
+    path[t++] = 'z';
+
+    return t;
 }
 
 int main() {
